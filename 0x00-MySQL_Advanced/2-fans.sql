ee652fd1623292fec51cb6114976368936865208
kingsantus <nkingsantus@gmail.com>
2024-07-11 21:48:22 +0000

    creating colunm and rows from existing file

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,6 +1,6 @@
 -- Rank country origins by the number of fans from metal bands file
 -- origin first then fans_number last
-SELECT origin, SUM(fans) AS fans_number
+SELECT origin, SUM(fans) AS nb_fans
 FROM metal_bands
 GROUP BY origin
-ORDER BY fans_number DESC;
+ORDER BY nb_fans DESC;
